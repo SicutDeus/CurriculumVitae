a81741ed2e5c64324809264025ea7a030c75d1f0
Михаил Присяжный <indawintaz@yandex.ru>
2022-11-08 20:10:23 +0300

    initial commit

---
+++ config/config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('pages.urls', namespace='pages')),
+    path('about/', include('pages.urls', namespace='pages')),
+    path('admin/', admin.site.urls),
+]
